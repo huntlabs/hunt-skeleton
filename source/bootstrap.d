ed44240f9ba628130fab1255337d8dc2641e4ddc
donglei <donglei@donglei.com>
2016-07-20 13:55:19 +0800

    change bootstrap

---
+++ source/bootstrap.d
@@ -0,0 +1,11 @@
+module bootstrap;
+
+import hunt.application;
+//import application.middleware;
+
+void main()
+{
+    auto app = Application.app();
+    //app.setMiddlewareFactory(new MiddlewareFactory()).enableLocale();
+    app.run();
+}
