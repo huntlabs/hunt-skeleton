39e3df6f2efefb352123fd5581a7668e92c720cb
Brian Zou <zoujiaqing@gmail.com>
2016-09-22 14:49:57 +0800

    Update index.d

--- source/app/controller/index.d
+++ source/app/controller/index.d
@@ -12,20 +12,4 @@ class IndexController : BaseController
         view.hello = "Hi";
         render!"index.html"();
     }
-    
-    @action
-    void helloworld()
-    {
-        response.html("Hello world!");
-    }
-    
-    @action
-    void json()
-    {
-        import std.json;
-        
-        JSONValue j = [ "hello": "world!" ];
-        
-        response.json(j);
-    }
 }
