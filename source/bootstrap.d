4a96a4dc0ccaf75d4f6b8703769d06da56be8abc
ZhangXuePing <zhangxueping@putao.com>
2018-05-30 19:24:18 +0800

    The conf file improved.

--- source/bootstrap.d
+++ source/bootstrap.d
@@ -1,8 +1,13 @@
 
 import hunt;
+import kiss.logger;
 
 void main()
 {
     auto app = Application.getInstance();
+    AppConfig config = Config.app;
+
+    logDebug(config.database.defaultOptions.url);
+
     app.run();
 }
