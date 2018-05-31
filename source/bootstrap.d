809c827981bc9f9a613cc27495ca57382a824dca
ZhangXuePing <zhangxueping@putao.com>
2018-05-31 09:26:49 +0800

    minor fixes

--- source/bootstrap.d
+++ source/bootstrap.d
@@ -1,13 +1,9 @@
 
 import hunt;
-import kiss.logger;
 
 void main()
 {
     auto app = Application.getInstance();
     AppConfig config = Config.app;
-
-    logDebug(config.database.defaultOptions.url);
-
     app.run();
 }
