7f4db706cc154a4fca86c44eaf612303bd45be4f
Brian Zou <zoujiaqing@gmail.com>
2018-07-10 02:28:55 +0800

    Change style

--- source/app/controller/IndexController.d
+++ source/app/controller/IndexController.d
@@ -9,16 +9,8 @@ class IndexController : Controller
     mixin MakeController;
 
     @Action
-    string index(int id)
+    string index()
     {
-        auto user = new User;
-
-        user.id = id;
-        user.name = "Brian";
-
-        // set user object to view
-        view.assign("user", user);
-
         // set huntVersion to view
         view.assign("huntVersion", HUNT_VERSION);
 
